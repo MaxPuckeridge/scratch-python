6919575a569307900a03c4fb79d2080e32b711fb
Max Puckeridge <max.puckeridge@gmail.com>
2016-06-09 22:53:02 +1000

    Better version of change problem

--- src/change/change.py
+++ src/change/change.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python
 
-import math
-
 # Set 6 - 2
 # http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-spring-2008/assignments/
 
 
 def make_change(denominations, C):
-    if C is 0:
-        return []
+    """ Returns an array of the change needed to make C from the set of possible denominations.
+        E.g.,
+          makeChange([1, 5, 10, 17] and 34)
+          >> [17, 17]
 
-    possible_combinations = []
-    for value in denominations:
-        if value is C:
-            possible_combinations.append([value])
-        elif value < C:
-            combination = [value]
-            combination.extend(make_change(denominations, C - value))
-            possible_combinations.append(combination)
+          makeChange([1, 5, 10, 17] and 33)
+          >> [1, 5, 10, 17]
+    """
 
-    if len(possible_combinations) is 0:
-        raise Exception("Change is smaller than the minimum denomination.")
+    # sorting helps us bail quicker in the for loop below.
+    denominations.sort()
 
-    min_combination = possible_combinations[0]
-    cumulative_min = len(min_combination)
-    for combination in possible_combinations:
-        current_min = len(combination)
-        if current_min < cumulative_min:
-            cumulative_min = current_min
-            min_combination = combination
+    # memoization of best_change
+    change_arr = {0: []}
 
-    return min_combination
+    def best_change(R):
+        if R not in change_arr:
+            min_change = None
+            for denomination in denominations:
+                if denomination == R:
+                    min_change = [denomination]
+                    break
+                elif denomination < R:
+                    this_change = [denomination] + best_change(R - denomination)
+                    if not min_change or len(min_change) > len(this_change):
+                        min_change = this_change
+                else:
+                    break
+            change_arr[R] = min_change
+        return change_arr[R]
 
+    # bottom up
+    for D in range(C):
+        best_change(D)
 
-if __name__ == '__main__':
-    print make_change()
+    return best_change(C)
